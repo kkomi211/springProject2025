3c4c5bbb7cfa671846b9dc24353a57357289f7bf
kkomi211 <cutybaby8024@gmail.com>
2025-10-24 16:25:48 +0900

    test

---
+++ springProject2025/src/main/java/com/example/springProject2025/SpringProject2025Application.java
@@ -0,0 +1,13 @@
+package com.example.springProject2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProject2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProject2025Application.class, args);
+	}
+
+}
---
+++ springProject2025/src/test/java/com/example/springProject2025/SpringProject2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springProject2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProject2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
